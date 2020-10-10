ad6e728bb8e5a2b026a0c711f331d7bdab9836f8
sadhu <sad.hu@outlook.com>
2020-10-10 13:51:27 +0800

    格式话代码

--- src/router/index.js
+++ src/router/index.js
@@ -18,10 +18,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue').catch(function(err) {
-      console.log({err})
-      return NullComponent
-    })
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue').catch(
+      function(err) {
+        console.log({err})
+        return NullComponent
+      }
+    )
     // component: About
   }
 ]
